2c134e18b4aff2a38525c8ff540fc81e2835996a
yifengzcs <yifengzcs@outlook.com>
2019-03-10 19:01:27 +0800

    Add files via upload

---
+++ header.js
@@ -0,0 +1,26 @@
+document.write('\
+    <div id="header">\
+      <img id="kyle" src="image/yifengz.jpg"><br>\
+      <div id=title>\
+        Yifeng ZHENG<br/>\
+        <span>PhD Student</span><br/><br/>\
+        <span class="address">Department of Computer\
+        Science</span><br/>\
+        <span class="address">City University of Hong Kong</span><br/>\<br/>\
+        <span class="address"><strong>Email:&nbsp;</strong>yifeng.zheng[at]my.cityu.edu.hk</span><br/>\
+        <span class="address"><a href="https://scholar.google.com/citations?user=QtAO6K0AAAAJ&hl=en">My Google Scholar Profile</a></span><br/> \
+      </div>\
+\
+      <div id="bmenu-container">\
+        <ul class="bmenu">\
+          <li id="about"><a href="index.html">About</a></li><!--\
+          --><li id="publications"><a\
+          href="publications.html">Publications</a></li><!--\
+          --><li id="teaching"><a\
+          href="teaching.html">Teaching</a></li><!--\
+          --><li id="honors"><a href="honors.html">Honors</a></li><!--\
+          --><li id="service"><a href="service.html">Services</a></li>\
+        </ul>\
+      </div> <!-- bmenu-container -->\
+    </div> <!-- header -->\
+');
