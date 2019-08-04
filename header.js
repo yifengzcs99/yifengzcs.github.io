bc713d8f76e422a8b37569955b37e14d76798b10
yifengzcs <yifengzcs@outlook.com>
2019-08-04 10:27:36 +0800

    Update header.js

--- header.js
+++ header.js
@@ -6,7 +6,7 @@ document.write('\
         <span>Research Fellow</span><br/>\
         <span class="address">Department of Computer\
         Science</span><br/>\
-        <span class="address">City University of Hong Kong</span><br/>\<br/>\
+        <span class="address"><a href="https://www.cityu.edu.hk/" target="_blank">City University of Hong Kong (CityU)</a></span><br/>\<br/>\
         <span class="address"><strong>Email:&nbsp;</strong>yifeng.zheng[at]my.cityu.edu.hk</span><br/><br/>\
         <span class="address"><a href="https://scholar.google.com/citations?user=QtAO6K0AAAAJ&hl=en">Google Scholar</a></span><br/> \
       </div>\
